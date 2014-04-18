f0677fd0775789935f0eef8f489e4465204770f7
Stephane Nicoll <snicoll@gopivotal.com>
2014-04-18 11:57:50 +0200

    Update guide to use @EnableJms
    This commit updates the guide to use @JmsListener and @EnableJms which
    is a new feature in Spring 4.1.
    
    This commit also slightly improve the definition in the build files as
    ActiveMQ is not required for compiling.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -8,45 +8,45 @@ import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Session;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.annotation.JmsListenerConfigurer;
+import org.springframework.jms.config.JmsListenerEndpointRegistrar;
+import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
-import org.springframework.jms.listener.SimpleMessageListenerContainer;
-import org.springframework.jms.listener.adapter.MessageListenerAdapter;
 import org.springframework.util.FileSystemUtils;
 
 @Configuration
+@EnableJms
 @EnableAutoConfiguration
-public class Application {
+public class Application implements JmsListenerConfigurer {
 
-    static String mailboxDestination = "mailbox-destination";
+    static final String mailboxDestination = "mailbox-destination";
+
+    @Autowired
+    private ConnectionFactory connectionFactory;
 
     @Bean
     Receiver receiver() {
         return new Receiver();
     }
 
-    @Bean
-    MessageListenerAdapter adapter(Receiver receiver) {
-        MessageListenerAdapter messageListener
-                = new MessageListenerAdapter(receiver());
-        messageListener.setDefaultListenerMethod("receiveMessage");
-        return messageListener;
+    @Override
+    public void configureJmsListeners(JmsListenerEndpointRegistrar registrar) {
+        registrar.setDefaultContainerFactory(jmsContainerFactory());
     }
 
     @Bean
-    SimpleMessageListenerContainer container(MessageListenerAdapter messageListener,
-                                             ConnectionFactory connectionFactory) {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setMessageListener(messageListener);
-        container.setConnectionFactory(connectionFactory);
-        container.setDestinationName(mailboxDestination);
-        container.setPubSubDomain(true);
-        return container;
+    SimpleJmsListenerContainerFactory jmsContainerFactory() {
+        SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
+        factory.setConnectionFactory(this.connectionFactory);
+        return factory;
     }
 
     public static void main(String[] args) {
--- complete/src/main/java/hello/Receiver.java
+++ complete/src/main/java/hello/Receiver.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.util.FileSystemUtils;
 
 public class Receiver {
@@ -18,6 +19,7 @@ public class Receiver {
      * When you receive a message, print it out, then shut down the application.
      * Finally, clean up any ActiveMQ server stuff.
      */
+    @JmsListener(destination = Application.mailboxDestination, queue = false)
     public void receiveMessage(String message) {
         System.out.println("Received <" + message + ">");
         context.close();
